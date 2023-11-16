9ef069cdc400717648889e16c1829a765778aa88
yericosan <149849880+u21230122@users.noreply.github.com>
2023-11-16 00:10:57 -0500

    Yericosan (#1)
    * aña
    
    * Completado-ControlProducto
    
    Completado-ControlProducto
    
    * NO FUNCIONA EL METODO DEL BOTON AGREGAR
    
    pipipi

--- Proyecto_Alquiler/src/main/java/Controlador/Inventariado/ControlProducto.java
+++ Proyecto_Alquiler/src/main/java/Controlador/Inventariado/ControlProducto.java
@@ -8,37 +8,95 @@ import DAO.Inventariado.ConsultaProducto;
 import Vista.Productos;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import DAO.Inventariado.ConsultaCategoria;
+import Modelo.Inventariado.Categoria_Mobiliario;
+import java.util.List;
+import javax.swing.JOptionPane;
 
-/**
- *
- * @author DAVID
- */
-public class ControlProducto implements ActionListener{
+import Modelo.Inventariado.Producto;
+
+public class ControlProducto implements ActionListener {
 
     private ConsultaProducto conPro;
     private Productos pro;
+    private ConsultaCategoria conCat;
 
-    public ControlProducto(ConsultaProducto conPro, Productos pro) {
+    public ControlProducto(ConsultaProducto conPro, Productos pro, ConsultaCategoria conCat) {
         this.conPro = conPro;
         this.pro = pro;
-        
-        pro.btn_agregar.addActionListener(this);
-        pro.btn_editarPre.addActionListener(this);
-        pro.btn_eliminar.addActionListener(this);
+        this.conCat = conCat;
+
+        this.pro.btn_agregar.addActionListener(this);
+        this.pro.btn_editarPre.addActionListener(this);
+        this.pro.btn_eliminar.addActionListener(this);
+        this.pro.cbo_CATE.addActionListener(this);
+        LLENARCOMBO();
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        if (e.getSource() == pro.btn_agregar) {
+            if (camposVacios()) {
+                JOptionPane.showMessageDialog(null, "Por favor, complete todos los campos obligatorios.");
+
+            } else {
+
+                Producto prod;
+                String nombre = pro.txt_nomb.getText();
+                String categoria = pro.cbo_CATE.getSelectedItem().toString();
+                String precio = pro.txt_precio.getText();
+                String cantidad = pro.txt_cant.getText();
+
+                
+
+               
+                prod = new Producto(nombre, categoria, precio, cantidad);
+                
+
+                if (conUs.nDocRepe(us.getNum_doc())) {
+                    JOptionPane.showMessageDialog(null, "Este numero de Documento ya esta registrado");
+                } else if (conUs.Registrar(us)) {
+                    JOptionPane.showMessageDialog(null, "Usuario Registrado correctamente");
+                    mostrarDatos();
+                    limpiar();
+                } else {
+                    JOptionPane.showMessageDialog(null, "Error en el registro del usuario");
+                    limpiar();
+                }
+            }
+
+        }
     }
-    
-    public void limpiar(){
-        
+
+    public void LLENARCOMBO() {
+
+        List<Categoria_Mobiliario> categorias = conCat.getListado();
+
+        String[] datos = new String[2];
+
+        for (Categoria_Mobiliario listacat : categorias) {
+
+            pro.cbo_CATE.addItem(listacat.getNom_cat());
+
+        }
+
+    }
+
+    public void limpiar() {
+
         pro.txt_nomb.setText("");
         pro.txt_cant.setText("");
         pro.txt_precio.setText("");
-        
+
     }
-    
-    
+
+    public boolean camposVacios() {
+        if (pro.txt_nomb.getText().isEmpty() || pro.txt_cant.getText().isEmpty()
+                || pro.txt_precio.getText().isEmpty()) {
+            return true; // Devuelve true si hay campos vacíos.
+        } else {
+            return false; // Devuelve false si no hay campos vacíos.
+        }
+    }
+
 }
